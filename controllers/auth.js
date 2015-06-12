392f6a3bd18ba374d338da2d8b8cfd06975c0df8
Dax Sorbito <daxsorbito@gmail.com>
2015-06-12 20:50:49 +0800

    added some tests

--- controllers/auth.js
+++ controllers/auth.js
@@ -35,7 +35,7 @@ var Auth = function(){
       yield next;
       var user_name = this.request.headers[config.HEADER.USER_NAME.toLowerCase()];
       yield redisStore.destroy(config.REDIS.PREFIX_KEY + ":USER_TOKEN:" + user_name);
-      setResponseBody(this, 200, {});
+      setResponseBody(this, 200);
       return this.body;
     }
   }
